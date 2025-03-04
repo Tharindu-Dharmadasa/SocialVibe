ca48220aa912d4a92f4036d4feef7463259902fb
Tharindu Dharmadasa <112732383+Tharindu-Dharmadasa@users.noreply.github.com>
2025-03-04 21:15:27 +0530

    Setup sign in and sign up from Clerk. And created database with postgres and prisma

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>Home Page Content</h1>
+    </div>
+  )
+}
+
+export default page;
